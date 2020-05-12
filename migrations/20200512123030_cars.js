6a5676ce42d25f94b8b0d47c8da84c6f3772f7ba
fjhansen <58237856+fjhansen@users.noreply.github.com>
2020-05-12 12:45:40 -0700

    migrations

---
+++ migrations/20200512123030_cars.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+  return knex.schema.table('cars', tbl => {
+    tbl.increments()
+
+    // VIN
+    tbl.string('VIN', 17).unique();
+    // MAKE
+    tbl.string('MAKE', 255)
+    // MODEL
+    tbl.string('MODEL', 255)
+    // MILEAGE
+    tbl.integer('MILEAGE', 255)
+    // TRANSMISSION TYPE
+    tbl.string("TRANSMISSION",255)
+    // STATUS OF TITLE 
+    tbl.string("TITLE", 255)
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('cars')
+};
