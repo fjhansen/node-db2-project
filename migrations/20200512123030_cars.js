7f2688ab716127afbb8e3855cb28fdcebacb4d85
fjhansen <58237856+fjhansen@users.noreply.github.com>
2020-05-12 12:59:04 -0700

    mvp

--- migrations/20200512123030_cars.js
+++ migrations/20200512123030_cars.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex) {
-  return knex.schema.table('cars', tbl => {
+  return knex.schema.createTable('cars', tbl => {
     tbl.increments()
 
     // VIN
--- routers/carRouter.js
+++ routers/carRouter.js
@@ -9,4 +9,26 @@ const db = require("../data/dbConnection");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  db('cars')
+  .then(cars => {
+    res.json(cars)
+  })
+  .catch(err => {
+    res.status(500).json({message: 'failed to retrieve cars'})
+  })
+})
+
+router.post('/', (req, res) => {
+  const theBody = req.body;
+  db('cars')
+  .insert(theBody)
+  .then(car => {
+    res.status(201).json({ data: car[0]})
+  })
+  .catch(err => {
+    res.status(500).json({ error: 'car could not be inserted'})
+  })
+})
+
 module.exports = router;
